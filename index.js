29f238bb75a732dca4d7c14ef61686ac015a7378
Guaguaaaa <guanyinghao223@gmail.com>
2024-04-11 02:20:29 +0100

    index.html footer added

---
+++ index.js
@@ -0,0 +1,21 @@
+const startDate = new Date('2023-04-20T20:43:00');
+
+function updateTimer() {
+    const currentDate = new Date();
+    const difference = currentDate - startDate;
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+
+    $("#days").html(days + "<span>Days</span>");
+    $("#hours").html(hours + "<span>Hours</span>");
+    $("#minutes").html(minutes + "<span>Minutes</span>");
+    $("#seconds").html(seconds + "<span>Seconds</span>");
+}
+
+setInterval(function() { updateTimer(); }, 1000);
